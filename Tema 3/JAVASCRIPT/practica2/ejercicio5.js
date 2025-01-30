f725d66e45ffe151eef6b225014fa9827ff11362
agascas2409 <agascas2409@g.educaand.es>
2025-01-30 13:50:26 +0100

    update

--- Tema 3/JAVASCRIPT/practica2/ejercicio5.js
+++ Tema 3/JAVASCRIPT/practica2/ejercicio5.js
@@ -1,9 +1,18 @@
 //Ejercicio 5
 
-let puntosActuales = 60;
-let cura = 30;
+let puntosActuales;
+let cura;
 
-Function curar(puntosActuales, cura){
+function curar(puntosActuales, cura){
     let puntos = Math.min(puntosActuales+cura, 100);
     return puntos;
 };
+
+let puntos = curar(50, 10);
+console.log(puntos);
+
+let puntos2 = curar(60, 20);
+console.log(puntos2);
+
+let puntos3 = curar(70, 30);
+console.log(puntos3);
