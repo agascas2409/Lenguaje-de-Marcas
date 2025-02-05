eea0f0816c76ae8bb7f363d97b663d6bb7dba100
agascas2409 <agascas2409@g.educaand.es>
2025-02-05 13:45:50 +0100

    Update codigo.js

--- Tema 3/JAVASCRIPT/Eventos/assets/codigo.js
+++ Tema 3/JAVASCRIPT/Eventos/assets/codigo.js
@@ -3,7 +3,7 @@ const b = document.querySelector("body");
 const p2 = document.querySelector("#niidea2");
 
 p1.addEventListener("click", () => alert('Parrafo 1 click'));
-b.addEventListener("load",() => mensaje);
+b.addEventListener("Load", mensaje());
 function mensaje(){
     alert("Pagina cargada");
 }
