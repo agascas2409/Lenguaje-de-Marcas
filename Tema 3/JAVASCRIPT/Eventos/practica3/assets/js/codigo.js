29f0ecb9d13da943c0a70d1f60a615261a156ba9
agascas2409 <agascas2409@g.educaand.es>
2025-02-10 09:10:14 +0100

    practica3

--- Tema 3/JAVASCRIPT/Eventos/practica3/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/Eventos/practica3/assets/js/codigo.js
@@ -7,15 +7,11 @@ const a4 = document.querySelector(".dobleClick");
 const a5 = document.querySelector(".boton");
 const a6 = document.querySelector(".cursor");
 
-//Creo variable para la capa 2
-let num1;
-let num2;
-
 //Al cargar la pagina aparece un mensaje
 b.addEventListener("Load", mensaje());
 function mensaje(){
     alert("Pagina cargada");
-}
+};
 
 //Al hacer click aparece un texto
 a1.addEventListener("click", () => alert('Has realizado un click'));
@@ -23,10 +19,31 @@ a1.addEventListener("click", () => alert('Has realizado un click'));
 //Se calcula el mayor de dos numeros introducidos
 a2.addEventListener("click", numeroMayor());
 function numeroMayor(){
-    
-}
+    const num1 = parseInt(prompt("Introduce un número: "));
+    const num2 = parseInt(prompt("Introduce un número: "));
+    if(!isNaN(num1) && !isNaN(num2)){
+        if(num1 > num2){
+            alert('El número máximo es ' + num1);
+        } else {
+            alert('El número máximo es ' + num2);
+        }
+    } else {
+        alert('El número no es valido');
+    }
+};
+
 //Se muestra por pantalla si un numero es par o impar cuando el cursor deje la imagen
-a3.addEventListener("click", () => alert('Has realizado un click'));
+a3.addEventListener("mouseleave", parOImpar());
+function parOImpar(){
+    const num = parseInt(prompt("Introduce un número: "));
+    if(!isNaN(num)){
+        if(num % 2 == 0){
+            alert('El número es par');
+        } else {
+            alert('El número es impar');
+        }
+    }
+};
 
 //Al hacer docle click aparece un texto
 a4.addEventListener("dblclick", () => alert('Has realizado un doble click'));
@@ -35,4 +52,14 @@ a4.addEventListener("dblclick", () => alert('Has realizado un doble click'));
 a5.addEventListener("click", () => alert('Has realizado un click'));
 
 //Al entrar el cursor en la capa se solicitan dos numeros y se realiza la multiplicacion
-a6.addEventListener("click", () => alert('Has realizado un click'));
+a6.addEventListener("mouseenter", multiplicacion());
+function multiplicacion(){
+    const num1 = parseInt(prompt("Introduce un número: "));
+    const num2 = parseInt(prompt("Introduce un número: "));
+    if(!isNaN(num1) && !isNaN(num2)){
+        let num3 = num1 * num2;
+        alert('La multiplicación de ' + num1 + ' y ' + num2 + ' es: ' + num3);
+    } else {
+        alert('El número no es valido');
+    }
+};
