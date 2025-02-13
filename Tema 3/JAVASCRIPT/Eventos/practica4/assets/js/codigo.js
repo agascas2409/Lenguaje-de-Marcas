d58b460fd0214652290d65ad96bc409ce1391383
agascas2409 <agascas2409@g.educaand.es>
2025-02-13 12:48:04 +0100

    practica4

---
+++ Tema 3/JAVASCRIPT/Eventos/practica4/assets/js/codigo.js
@@ -0,0 +1,15 @@
+//Creo todas las constantes 
+const a1 = document.querySelector(".color");
+const a2 = document.querySelector(".imagen");
+const a3 = document.querySelector(".edad");
+const a4 = document.querySelector(".cambiarColor");
+const a5 = document.querySelector(".cambiarEstilo");
+const a6 = document.querySelectorAll(".negro");
+
+//Cambia el color el parrafo al entrar y salir el raton
+a1.addEventListener("mouseenter", () => {a1.style.color="red"});
+a1.addEventListener("mouseleave", () => {a1.style.color="black"});
+
+//Cambiar la imagen cuando pase el cursor
+const nombreImagen = img.src.split("/").pop();
+a2.addEventListener("mouseenter", () => img.src="./assets/img/rosa");
