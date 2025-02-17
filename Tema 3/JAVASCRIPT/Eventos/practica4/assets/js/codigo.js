141c332dc424e4d08b78dfdc7145ed325e9732d6
agascas2409 <agascas2409@g.educaand.es>
2025-02-17 09:03:50 +0100

    practica4

--- Tema 3/JAVASCRIPT/Eventos/practica4/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/Eventos/practica4/assets/js/codigo.js
@@ -1,19 +1,45 @@
-//Creo todas las constantes 
+//Creo todas las constantes de cada capa
 const a1 = document.querySelector(".color");
 const a2 = document.querySelector(".imagen");
-const a3 = document.querySelector(".edad");
 const a4 = document.querySelector(".cambiarColor");
 const a5 = document.querySelector(".cambiarEstilo");
-const a6[] = document.querySelectorAll(".negro");
+const a6 = document.querySelectorAll(".negro");
+//Todo lo necesario para edad
+const a3 = document.querySelector(".edad");
+const boton = document.querySelector(".botonEdad");
+const texto = document.querySelector(".texto");
+const num = document.querySelector("#num");
 
 //Cambia el color el parrafo al entrar y salir el raton
 a1.addEventListener("mouseenter", () => {a1.style.color="red"});
 a1.addEventListener("mouseleave", () => {a1.style.color="black"});
 
 //Cambiar la imagen cuando pase el cursor
-//Aun no esta terminado
+//No funciona
 const nombreImagen = img.src.split("/").pop();
-if ()
-a2.addEventListener("mouseenter", () => img.src="./assets/img/rosa");
+a2.addEventListener("mouseenter", () => cambiarImagen(nombreImagen));
+function cambiarImagen(nombreImagen){
+    if (nombreImagen === "corazon.png"){
+        nombreImagen = img.src = "./assets/img/rosa.jpg";
+    } else {
+        nombreImagen = img.src = "./assets/img/corazon.png";
+    }
+}
 
-//
+//Comprueba que la edad esta en el rango
+//No funciona
+boton.addEventListener("click", () => numero(num, texto));
+function numero(num) {
+    let a=parseInt(num.value);
+    if (isNaN(num)){
+        texto.textContent = 'Introduce un número valido';
+    } else {
+        if(num >= 1 && num <= 100){
+            texto.textContent = 'Numero correcto';
+        } else {
+            texto.textContent = 'Introduce un valor entre 1 y 100';
+        }
+    }
+}
+
+//Cambia fondo negro o rojo
