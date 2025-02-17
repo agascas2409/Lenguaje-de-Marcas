0df902604e2919eb3c167ac2c219a4dd01a68da6
agascas2409 <agascas2409@g.educaand.es>
2025-02-17 08:26:59 +0100

    Update codigo.js

--- Tema 3/JAVASCRIPT/Eventos/practica4/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/Eventos/practica4/assets/js/codigo.js
@@ -4,12 +4,16 @@ const a2 = document.querySelector(".imagen");
 const a3 = document.querySelector(".edad");
 const a4 = document.querySelector(".cambiarColor");
 const a5 = document.querySelector(".cambiarEstilo");
-const a6 = document.querySelectorAll(".negro");
+const a6[] = document.querySelectorAll(".negro");
 
 //Cambia el color el parrafo al entrar y salir el raton
 a1.addEventListener("mouseenter", () => {a1.style.color="red"});
 a1.addEventListener("mouseleave", () => {a1.style.color="black"});
 
 //Cambiar la imagen cuando pase el cursor
+//Aun no esta terminado
 const nombreImagen = img.src.split("/").pop();
+if ()
 a2.addEventListener("mouseenter", () => img.src="./assets/img/rosa");
+
+//
