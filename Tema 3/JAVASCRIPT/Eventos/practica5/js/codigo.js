41d5a142fef4aef721bafa61d66da37a3b459083
agascas2409 <agascas2409@g.educaand.es>
2025-02-21 11:01:05 +0100

    Update codigo.js

--- Tema 3/JAVASCRIPT/Eventos/practica5/js/codigo.js
+++ Tema 3/JAVASCRIPT/Eventos/practica5/js/codigo.js
@@ -2,14 +2,10 @@
 const num1 = document.querySelector("#num1");
 const num2 = document.querySelector("#num2");
 const operacion = document.querySelector("#operacion");
-const suma = document.querySelector(".suma");
-const resta = document.querySelector(".resta");
-const multiplicacion = document.querySelector(".multiplicacion");
-const division = document.querySelector(".division");
 const resultado = document.querySelector("#resultado");
 const boton = document.querySelector(".boton");
 
-boton.addEventListener("click", () => calcular(num1, num2, operacion));
+boton.addEventListener("click", () => calcular(num1, num2, operacion.value);
 
 function calcular(num1, num2, operacion){
     let num1 = parseFloat(num1.value);
@@ -20,19 +16,19 @@ function calcular(num1, num2, operacion){
         resultado.style.color = "red";
     } else {
         switch(operacion){
-            case suma:
+            case 'suma':
                 resultadoOperacion = num1 + num2;
                 resultado.value = `${resultadoOperacion}`;
                 break;
-            case resta:
+            case 'resta':
                 resultadoOperacion = num1 - num2;
                 resultado.value = `${resultadoOperacion}`;
                 break;
-            case multiplicacion:
+            case 'multiplicacion':
                 resultadoOperacion = num1 * num2;
                 resultado.value = `${resultadoOperacion}`;
                 break;
-            case division:
+            case 'division':
                 resultadoOperacion = num1 / num2;
                 resultado.value = `${resultadoOperacion}`;
                 break;
