564c732e759e94fb9523d59046020baaef60119f
agascas2409 <agascas2409@g.educaand.es>
2025-03-06 12:42:00 +0100

    cambios

--- Tema 3/JAVASCRIPT/ProcesaJSON/practica6.1/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/ProcesaJSON/practica6.1/assets/js/codigo.js
@@ -9,26 +9,24 @@ async function buscarPelicula() {
 
     const titulo = document.querySelector(".titulo");
     const anyo = document.querySelector(".anyo");
-    const duraccion = document.querySelector("duraccion");
+    const duracion = document.querySelector(".duracion");
 
-    //Capturamos los posibles errores
-    try {
-        //Obtenemos la respuesta
-        const respuesta = await fetch(url);
-        //Procesamos la respuesta a un objeto data javascript
-        const data = await respuesta.json();
-
+    //Obtenemos la respuesta
+    const respuesta = await fetch(url);
+    //Procesamos la respuesta a un objeto data javascript
+    const data = await respuesta.json();
+    
+    if (data.Title) {
         //Mostramos los datos en las casillas correspondientes
         titulo.innerHTML = `<p>${data.Title}</p>`;
         anyo.innerHTML = `<p>${data.Year}</p>`;
-        duraccion.innerHTML = `<p>${data.Runtime}</p>`;
-
-    } catch (error) {
+        duracion.innerHTML = `<p>${data.Runtime}</p>`;
+    } else {
         //Mostramos que no se ha encontrado la pelicula y los posibles errores que puedan suceder
         titulo.innerHTML = `No se encuentra la pelicula`;
         titulo.style.color = "red";
         anyo.innerHTML = ``;
-        duraccion.innerHTML = ``;
+        duracion.innerHTML = ``;
         console.error("Error al obtener datos", error);
     }
 
