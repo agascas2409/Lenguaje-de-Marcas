52ee94e90be6a90d77d2c3df161879ba31d11597
agascas2409 <agascas2409@g.educaand.es>
2025-03-18 23:02:45 +0100

    cambios

--- Tema 3/JAVASCRIPT/ProcesaJSON/practica6.3/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/ProcesaJSON/practica6.3/assets/js/codigo.js
@@ -1,6 +1,9 @@
 const button = document.querySelector("#buscar");
 button.addEventListener("click", buscarPersonaje);
 
+const buttonEpisodio = document.querySelector("#buscarEpisodio");
+buttonEpisodio.addEventListener("click", buscarEpisodio);
+
 async function buscarPersonaje() {
 
     const aBuscar = document.querySelector("#personaje").value;
@@ -38,6 +41,37 @@ async function buscarPersonaje() {
         genero.innerHTML="";
         imagen.src="";
         episodios.innerHTML="";
+    }
+
+}
+
+async function buscarEpisodio() {
+
+    const aBuscar = document.querySelector("#episodio").value;
+    //Creamos la url colocando el texto de búsqueda en su sitio
+    const url = `https://rickandmortyapi.com/api/episode/${aBuscar}`;
+
+    const foto = document.querySelector(".foto");
+    const personajesEpisodio = document.querySelector(".personajesEpisodio");
+    personajesEpisodio.innerHTML="";
+
+    //Obtenemos la respuesta
+    const respuesta = await fetch(url);
+    //Procesamos la respuesta a un objeto data javascript
+    const data = await respuesta.json();
+
+    try {
+        //No existe ninguna imagen en el json de episodios
+        foto.src=`${data.url}`;
+        for (let index = 0; index < data.characters.length; index++) {
+            const respuesta1 = await fetch(data.characters[index]);
+            const data2 = await respuesta1.json();
+            personajesEpisodio.innerHTML = personajesEpisodio.innerHTML + `<li>${data2.name}</li>`;
+        }
+
+    } catch (error) {
+        foto.src="";
+        personajesEpisodio.innerHTML="";
     }
 
 }
