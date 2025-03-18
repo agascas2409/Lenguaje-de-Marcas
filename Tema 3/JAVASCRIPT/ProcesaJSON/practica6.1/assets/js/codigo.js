e9e028a816d298de7ca62f9bb2ab034eadbf5e2f
agascas2409 <agascas2409@g.educaand.es>
2025-03-18 22:07:01 +0100

    cambios

--- Tema 3/JAVASCRIPT/ProcesaJSON/practica6.1/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/ProcesaJSON/practica6.1/assets/js/codigo.js
@@ -1,6 +1,14 @@
 const button = document.querySelector("#buscar");
 button.addEventListener("click", buscarPelicula);
 
+const medio = document.querySelector(".medio");
+const valor = document.querySelector(".valor");
+const valoracion = document.querySelector(".valoracion");
+valoracion.innerHTML = ``;
+const valoresDesplegable = new Map();
+
+valoracion.addEventListener("click", actualizarRating);
+
 async function buscarPelicula() {
 
     const aBuscar = document.querySelector("#pelicula").value;
@@ -22,6 +30,15 @@ async function buscarPelicula() {
         titulo.innerHTML = `<p>${data.Title}</p>`;
         anyo.innerHTML = `<p>${data.Year}</p>`;
         duracion.innerHTML = `<p>${data.Runtime}</p>`;
+        
+
+        for (let index = 0; index < data.Ratings.length; index++) {
+            valoracion.innerHTML = valoracion.innerHTML + `<option>${data.Ratings[index].Source}</option>`;
+            valoresDesplegable.set(data.Ratings[index].Source, data.Ratings[index].Value);
+        }
+        
+        actualizarRating();
+
     } else {
         //Mostramos que no se ha encontrado la pelicula y los posibles errores que puedan suceder
         titulo.innerHTML = `No se encuentra la pelicula`;
@@ -30,14 +47,9 @@ async function buscarPelicula() {
         duracion.innerHTML = ``;
         console.error("Error al obtener datos", error);
     }
+}
 
-    const medio = document.querySelector(".medio");
-    const valor = document.querySelector(".valor");
-    const valoracion = document.querySelector("#valoracion");
-    valoracion.innerHTML = ``;
-
-    for (let index = 0; index < data.Ratings.length; index++) {
-        valoracion.innerHTML = valoracion.innerHTML + `<option>${element.Ratings[index].Source}</option>`;
-    }
-
+function actualizarRating(){
+    medio.innerHTML = `${valoracion.value}`;
+    valor.innerHTML = `${valoresDesplegable.get(valoracion.value)}`;
 }
