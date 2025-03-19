9339c5778c0ded76c214a7c766f5143588e02a27
agascas2409 <agascas2409@g.educaand.es>
2025-03-19 13:16:05 +0100

    examen

--- Tema 3/JAVASCRIPT/examen/opcion3/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/examen/opcion3/assets/js/codigo.js
@@ -1,13 +1,17 @@
+//Declaramos el boton y le añadimos un evento
 const button = document.querySelector("#buscar");
 button.addEventListener("click", buscarPalabra);
 
+//Declaramos el desplegable y le añadimos un evento al cambiar la seleccion del desplegable
 const definiciones = document.querySelector("#definiciones");
 definiciones.addEventListener("change", actualizarDesplegable);
 definiciones.innerHTML="";
 
+//Declaramos donde vamos a mostrar las definiciones
 const definicion = document.querySelector(".definicion");
 definicion.innerHTML="";
 
+//Creamos un mapa
 const desplegable = new Map();
 
 async function buscarPalabra() {
@@ -28,19 +32,25 @@ async function buscarPalabra() {
         palabraBuscada.innerHTML = fichero.data.word;
         palabraBuscada.style.color = `green`;
         definiciones.innerHTML="";
+        //Utilizammos un for para guardar el numero de definiciones que tiene la palabra buscada
+        //Ademas de guardarlo en el mapa para despues poder acceder a esa información
         for (let index = 0; index < fichero.data.meanings[0].senses.length; index++) {
             definiciones.innerHTML = definiciones.innerHTML + `<option>${index+1}</option>`;
             desplegable.set((index), fichero.data.meanings[0].senses[index].raw);
         }
 
+        //Para que se muestre la definición 1 en el momento en el que se busque la palabra
         actualizarDesplegable();
     } else {
         //Mostramos que no se ha encontrado la palabra y los posibles errores que puedan suceder
         palabraBuscada.innerHTML = `Palabra no encontrada`;
         palabraBuscada.style.color = `red`;
+        definiciones.innerHTML="";
+        definicion.innerHTML ="";
     }
 }
 
 function actualizarDesplegable(){
+    //Actualizamos el desplegable dependiendo de la opción que haya elegido el usuario
     definicion.innerHTML = `${desplegable.get(definiciones.value-1)}`;
 }
