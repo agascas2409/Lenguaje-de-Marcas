139db511f5af32f7f91c123b8c7ee408d357be92
agascas2409 <agascas2409@g.educaand.es>
2025-03-20 09:40:10 +0100

    cambios

--- Tema 3/JAVASCRIPT/practica7/js/codigo.js
+++ Tema 3/JAVASCRIPT/practica7/js/codigo.js
@@ -1,42 +1,80 @@
 const numeros = document.querySelectorAll(".boton");
 const operaciones = document.querySelectorAll(".operacion");
-const limpiar = document.querySelector(".limpiar");
+const limpia = document.querySelector(".limpia");
 const solucion = document.querySelector("#solucion");
 const display = document.querySelector("#display");
 
-let primerOP;
-let segundoOP;
-let operacion = "";
-let operandoActual;
+let resultado;
+let operando = "";
+let operandoActual = "";
+let operandoGuardado = "";
+
+solucion.addEventListener("click", calcularOperacion);
+limpia.addEventListener("click", borrar);
 
 numeros.forEach(numero => {
     numero.addEventListener("click", () => {
-        if (operacion != "") {
-            display.value = "";
-            operandoActual = numero.value;
+        if (!operandoActual.includes(".") || operandoActual.includes(".") && numero.value != ".") {
+            operandoActual = operandoActual + numero.value;
             display.value = operandoActual;
         } else {
-            
+            display.value = "Un numero no puede tener dos puntos";
+            actualizar();
         }
     })
 });
 
-
-
-
-
-
-
-
-
 operaciones.forEach(operacion => {
-    operacion.addEventListener("click", )
+    operacion.addEventListener("click", () => {
+        operando = operacion.value;
+        display.value = operacion.value;
+        operandoGuardado = operandoActual;
+        operandoActual = "";
+    })
 });
 
-
-
-solucion.addEventListener("click", calcularOperacion);
-
 function calcularOperacion() {
-    
+    switch (operando) {
+        case "+":
+            resultado = operandoGuardado + operandoActual;
+            actualizar();
+            display.value = resultado;
+            break;
+        case "-":
+            resultado = operandoGuardado - operandoActual;
+            actualizar();
+            display.value = resultado;
+            break;
+        case "x":
+            resultado = operandoGuardado * operandoActual;
+            actualizar();
+            display.value = resultado;
+            break;
+        case "/":
+            if (operandoActual == 0) {
+                display.value = "El divisor no puede ser 0";
+            } else {
+                resultado = operandoGuardado / operandoActual;
+                actualizar();
+                display.value = resultado;
+            }
+            break;
+        default:
+            display.value = "No se a seleccionado operación";
+            break;
+    }
+}
+
+function borrar() {
+    display.value = "";
+    resultado = "";
+    operando = "";
+    operandoActual = "";
+    operandoGuardado = "";
+}
+
+function actualizar(){
+    operandoGuardado = "";
+    operandoActual = "";
+    operando = "";
 }
